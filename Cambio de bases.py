5b26613f157cfc8cccf4990d26c177736adcb7a8
Jeff-12 <60985537+Jeff-12@users.noreply.github.com>
2020-03-02 17:29:59 -0600

    (Cambio bases 10 a otra)

--- Cambio de bases.py
+++ Cambio de bases.py
@@ -1,2 +1,7 @@
-print(10)
-print (1)
+def aCadena(n,base):
+    Tabla:"012345678"
+    if n<base:
+        return Tabla[n]
+    else:
+        return aCadena(n//base,base)+Tabla[n%base]
+print(aCadena(12,2)) 
