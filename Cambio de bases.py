8597b1c810ffd816fef0436cc6a6f88ad5a7b3d8
Jeff-12 <60985537+Jeff-12@users.noreply.github.com>
2020-03-02 17:42:23 -0600

    (correcion)

--- Cambio de bases.py
+++ Cambio de bases.py
@@ -1,7 +1,11 @@
 def aCadena(n,base):
-    Tabla:"012345678"
+    Tabla="012345678"
     if n<base:
-        return Tabla[n]
+         return Tabla[n]
+    if base==0:
+        return 0
+    if base==1:
+        return 1
     else:
         return aCadena(n//base,base)+Tabla[n%base]
-print(aCadena(12,2)) 
+print(aCadena(12,10)) 
