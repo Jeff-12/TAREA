b9a112712ed6e6cd90e77739403fd94375df581d
Jeff-12 <60985537+Jeff-12@users.noreply.github.com>
2020-03-03 18:51:33 -0600

    (Cambios de intervalo de bases)

--- Cambio de bases.py
+++ Cambio de bases.py
@@ -2,10 +2,6 @@ def aCadena(n,base):
     Tabla="012345678"
     if n<base:
          return Tabla[n]
-    if base==0:
-        return 0
-    if base==1:
-        return 1
     else:
         return aCadena(n//base,base)+Tabla[n%base]
 print(aCadena(12,10)) 
