5f0dbdb6884100d62217c4c2b33b021a880a541e
TheKiller48 <angellomg81@gmail.com>
2020-03-04 13:36:02 -0600

    cambio de variables

--- Cambio de bases.py
+++ Cambio de bases.py
@@ -5,3 +5,11 @@ def aCadena(n,base):
     else:
         return aCadena(n//base,base)+Tabla[n%base]
 print(aCadena(12,10)) 
+def cambio(a,b):
+    return cambio_aux(a,b,0,0)
+def cambio_aux(a,b,n,m):
+    if a>0:
+        return cambio_aux(a//10,b,n+1,((a%10)*(b**n))+m)
+    else:
+        return m
+print (cambio(1010001110,2))
