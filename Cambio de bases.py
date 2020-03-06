dd6aa46e8a74356136da8b4a7f11e06053b73a82
TheKiller48 <angellomg81@gmail.com>
2020-03-05 21:48:11 -0600

    Archivo final 2.1

--- Cambio de bases.py
+++ Cambio de bases.py
@@ -1,17 +1,19 @@
+def test_aCadena():
+    assert aCadena(10,5)==20
+    assert aCadena(20,7)==26
 def aCadena(n,base):
     Tabla="012345678"
     if n<base:
          return Tabla[n]
     else:
         return aCadena(n//base,base)+Tabla[n%base]
-<<<<<<< HEAD
-def test_aCadena():
-    assert aCadena(10,5)==20
-    assert aCadena(20,7)==26
 print(aCadena(23,10))
-print(aCadena(14,6))
+print(aCadena(15,6))
 
 
+def test_cambio():
+    assert cambio(13,8)==11
+    assert cambio(20,6)==12
 def cambio(a,b):
     return cambio_aux(a,b,0,0)
 def cambio_aux(a,b,n,m):
@@ -19,11 +21,5 @@ def cambio_aux(a,b,n,m):
         return cambio_aux(a//10,b,n+1,((a%10)*(b**n))+m)
     else:
         return m
-def test_cambio():
-    assert cambio(13,8)==11
-    assert cambio(20,6)==12
 print (cambio(1010001110,2))
 print (cambio(3201,4))
-=======
-print(aCadena(12,10))
->>>>>>> f523ce24ece2ca544aed51ec2a08d6338f379706
